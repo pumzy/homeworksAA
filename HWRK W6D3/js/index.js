d3577c87801fa3cf12f5c90352dbf742e8480a31
Amaar Esufally <amaar.esufally@gmail.com>
2017-06-06 19:39:27 -0400

    Homework for W6D3

---
+++ HWRK W6D3/js/index.js
@@ -0,0 +1,20 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+$.ajax({
+  type : 'GET',
+  url : 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log("The weather is");
+    console.log(data);
+  
+  },
+  error() {
+    console.log("There was a problem");
+  }
+});
+
+
+
+// Add another console log here, outside your AJAX request
